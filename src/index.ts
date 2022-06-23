70fb708297f67725a8159be90ba9220ef8f5355b
Éverton M. Vieira <emuvi@outlook.com.br>
2022-06-23 20:51:53 -0300

    call freeze detection and parses the results

--- src/index.ts
+++ src/index.ts
@@ -42,11 +42,75 @@ class Inmuvi extends QinStack {
     this._qinText.appendLine("Starting Inmuvi...");
     this._qinText.appendLine("Input: " + input);
     this._qinText.appendLine("Output: " + output);
+    this.step1CallFreezeDetection({ input, output, tokens: new Map() });
   }
 
   private actBack() {
     this.show(this._qinBody);
   }
+
+  private step1CallFreezeDetection(process: Process) {
+    this.qinpel.talk.cmd
+      .run({
+        exec: "ffmpeg",
+        args: [
+          "-i",
+          process.input,
+          "-vf",
+          "freezedetect=n=0.001:d=1",
+          "-map",
+          "0:v:0",
+          "-f",
+          "null",
+          "-",
+        ],
+      })
+      .then((token) => {
+        this._qinText.appendLine("Started on: " + token);
+        process.tokens["FreezeDetection"] = token;
+        this.step2ParseFreezeDetected(process);
+      })
+      .catch((err) => {
+        this._qinText.appendLine("Error on Start: " + err.message);
+      });
+  }
+
+  private step2ParseFreezeDetected(process: Process) {
+    this.qinpel.talk.issued
+      .askWhenDone({
+        token: process.tokens["FreezeDetection"],
+        askResultLines: true,
+      })
+      .then((res) => {
+        if (res.resultLines) {
+          for (let line of res.resultLines) {
+            line = line.toLowerCase();
+            if (line.startsWith("[freezedetect @")) {
+              let posStart = line.indexOf(".freeze_start:");
+              if (posStart > -1) {
+                let freezeStart = line.substring(posStart + 14).trim();
+                this._qinText.appendLine("Freeze Start: " + freezeStart);
+              } else {
+                let posEnd = line.indexOf(".freeze_end:");
+                if (posEnd > -1) {
+                  let freezeEnd = line.substring(posEnd + 12).trim();
+                  this._qinText.appendLine("Freeze End: " + freezeEnd);
+                }
+              }
+            }
+          }
+        }
+      })
+      .catch((err) => {
+        this._qinText.appendLine("Error on Show: " + err.message);
+      });
+  }
 }
+
+type Process = {
+  input: string;
+  output: string;
+  tokens: Map<string, string>;
+};
 
 new Inmuvi().style.putAsBody();
